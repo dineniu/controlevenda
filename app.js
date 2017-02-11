40544ae4c8d4eebb76182bd108aff52d4ed6aef6
dineniu <dine_niu@hotmail.com>
2017-02-11 15:42:42 -0200

    Update app.js

--- app.js
+++ app.js
@@ -20,11 +20,12 @@ mongoose.connect('mongodb://admin:212230@jello.modulusmongo.net:27017/e5rEqazi')
 var db = mongoose.connection;
 
 app.get('/',function(req, res){
-        var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
+	res.redirect('http://makeiteasy.herokuapp.com');
+        /*var obj = fs.readFileSync(path.join(publicDir, 'index.html'), 'utf8');
 	if(req.query.lang == "pt"){
 	  obj = obj.replace("./verify", "./verificar").replace("lang: 'en'", "lang: 'pt'").replace("SEND CAPTCHA","ENVIAR CAPTCHA");
 	}
-	 res.send(obj);
+	 res.send(obj);*/
 });
 app.get('/pt',function(req, res){
 	var string = encodeURIComponent('pt');
